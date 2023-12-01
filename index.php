a7b83fb2ea552320c07739e5ef637a017de63df0
rumibalazs <balazs.rumi23@gmail.com>
2023-12-01 16:19:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css.css">
+    <script src="app.js" defer></script>
+
+    <title></title>
+</head>
+
+<body>
+
+    
+<main>
+    <button id="prev">
+        <i class="material-icons">&#xe5c4;</i>
+    </button>
+    <div id="book" class="book">
+        <div id="p1" class="paper">
+            <div class="front">
+                <div id="f1" class="front_content">
+                    <h1>FRONT1</h1>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b1" class="back_content">
+                    <h1>IMAGE</h1>
+                </div>
+            </div>
+        </div>
+
+        <div id="p2" class="paper">
+            <div class="front">
+                <div id="f2" class="front_content">
+                    <h1>süti neve</h1>
+                    <h2>hozzávalók</h2>
+                    <h3>recept</h3>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b2" class="back_content">
+                    <h1>BACK2</h1>
+                </div>
+            </div>
+        </div>
+
+        <div id="p3" class="paper">
+            <div class="front">
+                <div id="f3" class="front_content">
+                    <h1>FRONT3</h1>
+                </div>
+            </div>
+            <div class="back">
+                <div id="b3" class="back_content">
+                    <h1>BACK3</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <button id="next">
+        <i class="material-icons">&#xe5c8;</i>
+    </button>
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
